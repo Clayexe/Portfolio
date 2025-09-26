d9d890b882dfd20dd8712c884828d5dec01e70e5
Clayexe <77508458+Clayexe@users.noreply.github.com>
2025-09-25 20:21:30 -0400

    Add files via upload

---
+++ random treasure.py
@@ -0,0 +1,8 @@
+def treasure_chest():
+    gold=random.randint(1,10)
+    print("The treasure chest contains", gold, "pieces of gold")
+#Main program begins here
+import random
+player_gold=5
+print("I found a treasure chest!")
+treasure_chest() # This line of code calls the treasure_chest function
