fb127b9e05390391c7c93b45b78dba0487c6855e
Clayexe <77508458+Clayexe@users.noreply.github.com>
2025-09-25 20:23:19 -0400

    Update random treasure.py

--- random treasure.py
+++ random treasure.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep 25 17:59:31 2025
+
+@author: Clay Smith
+"""
 def treasure_chest():
     gold=random.randint(1,10)
     print("The treasure chest contains", gold, "pieces of gold")
@@ -5,4 +11,5 @@ def treasure_chest():
 import random
 player_gold=5
 print("I found a treasure chest!")
+
 treasure_chest() # This line of code calls the treasure_chest function
